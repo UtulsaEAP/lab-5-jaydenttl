d4c96c893d0590132fdcf1610ef68c9107f3eebb
Jayden Ly <jtl5627@utulsa.edu>
2024-02-29 14:03:17 -0600

    5

--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -2,7 +2,7 @@
 Name: Jayden Ly
 Lab time: thursday 11:57 am
 '''
-def swap_values(user_val1, user_val2, user_val3, user_val4):   
+def swap_values(user_input1, user_input2, user_input3, user_input4):   
    #write your code here
    user_val1 = user_input2
    user_val2 = user_input1
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
    user_val1, user_val2, user_val3, user_val4 = swap_values(user_input1, user_input2, user_input3, user_input4)
 
-   print(user_val1, user_val2, user_input3, user_input4)
+   print(user_val1, user_val2, user_val3, user_val4)
    #store output for every input here
    #print those output
 
