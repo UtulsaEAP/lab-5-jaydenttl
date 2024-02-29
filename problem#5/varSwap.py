595c1541769784ffdf6d279a9146957b4836dbb9
Jayden Ly <jtl5627@utulsa.edu>
2024-02-29 12:40:19 -0600

    5 6

--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -1,6 +1,13 @@
+'''
+Name: Jayden Ly
+Lab time: thursday 11:57 am
+'''
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   
+   user_val1 = user_input2
+   user_val2 = user_input1
+   user_val3 = user_input4
+   user_val4 = user_input3
    return user_val1, user_val2, user_val3, user_val4
 
 if __name__ == '__main__':   
@@ -8,6 +15,10 @@ if __name__ == '__main__':
    user_input2 = int(input())
    user_input3 = int(input())
    user_input4 = int(input())
+
+   user_val1, user_val2, user_val3, user_val4 = swap_values(user_input1, user_input2, user_input3, user_input4)
+
+   print(user_val1, user_val2, user_input3, user_input4)
    #store output for every input here
    #print those output
 
--- problem#6/fib.py
+++ problem#6/fib.py
@@ -1,5 +1,34 @@
+''' 
+Name: Jayden Ly
+Lab Time: thurs 12:09 PM
+
 def fibonacci(n):
-    #write your code here
+    if n > 0:
+        term = 0
+        newterm = 1
+        for _ in (0, n + 1):
+            term, newterm = newterm, term + newterm
+            return newterm
+        
+    elif n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+          return -1
+'''
+def fibonacci(n):
+    if n > 1:
+        old, new = 0, 1
+        for _ in range(2, n + 1):
+            old, new = new, old + new
+        return new
+    elif n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return -1
 
 
 if __name__ == '__main__':
